3cc46b4d433f98b7e83430c769d83d2e75d50fc8
NataliaD <natalia123doroz@gmail.com>
2020-06-18 16:14:46 +0200

    chande methods type to void

--- src/TimeMachine.java
+++ src/TimeMachine.java
@@ -1,11 +1,9 @@
 public class TimeMachine {
-    int backInTime(Person person) {
+    void backInTime(Person person) {
         person.age = person.age - 1;
-        return person.age;
     }
 
-    int timeTravel(Person person, int number) {
+    void timeTravel(Person person, int number) {
         person.age = person.age + number;
-        return person.age;
     }
 }
